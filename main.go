b8368231878b8c710a2d2e5cc03914bd9ac002af
dayounglee <verdantjuly@kakao.com>
2024-04-04 17:28:49 +0900

    feat : init commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"minevillages/monitoring/api"
+	baseapi "minevillages/monitoring/api/base"
+	"net/http"
+	"runtime"
+	// "github.com/quic-go/quic-go/http3"
+)
+
+func main() {
+	// 프로세스의 모든 CPU 코어를 활용하도록 설정합니다.
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	handler := api.HostRouteHandler{}
+	handler.RegisterHost("127.0.0.1:8080", baseapi.HTTPHandler{})
+
+	if err := http.ListenAndServe(
+		":8080",
+		// "ssl/certificate.crt",
+		// "ssl/private.key",
+		&handler,
+	); err != nil {
+		log.Fatalln("HTTP 관련 수신 소켓을 초기화하는 과정에서 예외가 발생하였습니다.", err.Error())
+	}
+}
